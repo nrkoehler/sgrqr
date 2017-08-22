f58a9a92e7bf1312d779914dc575ffedfd6a84b6
nrkoehler <norbert.r.koehler@gmail.com>
2017-08-22 13:19:29 +0200

    edit data

--- R/data.R
+++ R/data.R
@@ -12,11 +12,6 @@ colnames(df.full) <- c('id', c(paste0("sgrq.", 1:8),
                                paste0("sgrq.14", letters[1:4]), 
                                paste0("sgrq.16", letters[1:5]), 'sgrq.17'))
 df.full$id <- as.character(c(1:100))
-df.full[1, 2:10] <- 1
-df.full[1, 11] <- 2
-df.full[1, 12:50] <- 1
-df.full[1, 51] <- 3
-
 
 for (i in 2:8){
   df.full[i] = round(runif(100, min = 1, max = 5))
@@ -30,6 +25,7 @@ df.full[10] <- round(runif(100, min = 1, max = 3))
 df.full[11] <- round(runif(100, min = 0, max = 2))
 df.full[51] <- round(runif(100, min = 0, max = 3))
 
+df.full[1, 2:8] <- 1
 
 #' data frame with missing values
 #' @export
@@ -38,6 +34,7 @@ for (i in 2:ncol(df.na)){
 df.na[sample(2:nrow(df.na), 10), i] <- NA
 }
 rm(i)
+
 #' Regression weights for SGRQ items
 #'
 #' A data frame containing the regression weights for SGRQ items
