6ba0a91d4b7b4ec85a332e40b226b142944a643f
nrkoehler <norbert.r.koehler@gmail.com>
2017-08-22 13:13:57 +0200

    edit data

--- R/data.R
+++ R/data.R
@@ -5,8 +5,8 @@
 
 df.full <- data.frame(matrix(ncol=51, nrow=100))
 colnames(df.full) <- c('id', c(paste0("sgrq.", 1:8), 
-                               paste0("sgrq11.", letters[1:7]), 
-                               paste0("sgrq15.", letters[1:9]),
+                               paste0("sgrq.11", letters[1:7]), 
+                               paste0("sgrq.15", letters[1:9]),
                                'sgrq.9', 'sgrq.10',
                                paste0("sgrq.12", letters[1:6]), paste0("sgrq.13", letters[1:8]),
                                paste0("sgrq.14", letters[1:4]), 
@@ -37,7 +37,7 @@ df.na <- df.full
 for (i in 2:ncol(df.na)){
 df.na[sample(2:nrow(df.na), 10), i] <- NA
 }
-
+rm(i)
 #' Regression weights for SGRQ items
 #'
 #' A data frame containing the regression weights for SGRQ items
