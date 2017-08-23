7cb6e252555058da2ca8ca627839d10644ab7d7d
nrkoehler <norbert.r.koehler@gmail.com>
2017-08-23 20:16:53 +0200

    edit functions

--- R/data.R
+++ R/data.R
@@ -4,26 +4,28 @@
 
 
 df.full <- data.frame(matrix(ncol=51, nrow=100))
-colnames(df.full) <- c('id', paste0('sgrq.', seq(1, 50, 1)))
+colnames(df.full) <- c('id', c(paste0("sgrq.", 1:8), 
+                               paste0("sgrq.11", letters[1:7]), 
+                               paste0("sgrq.15", letters[1:9]),
+                               'sgrq.9', 'sgrq.10',
+                               paste0("sgrq.12", letters[1:6]), paste0("sgrq.13", letters[1:8]),
+                               paste0("sgrq.14", letters[1:4]), 
+                               paste0("sgrq.16", letters[1:5]), 'sgrq.17'))
 df.full$id <- as.character(c(1:100))
-df.full[1, 2:10] <- 1
-df.full[1, 11] <- 2
-df.full[1, 12:50] <- 1
-df.full[1, 51] <- 3
 
 
-for (i in 2:8){
+for (i in c(2:8)){
   df.full[i] = round(runif(100, min = 1, max = 5))
 }
 
-for (i in c(9, 12:50)){
+for (i in c(9:25, 28:50)){
   df.full[i] = round(runif(100, min = 0, max = 1))
 }
 
-df.full[10] <- round(runif(100, min = 1, max = 3))
-df.full[11] <- round(runif(100, min = 0, max = 2))
+df.full[26] <- round(runif(100, min = 0, max = 3))
+df.full[27] <- round(runif(100, min = 0, max = 2))
 df.full[51] <- round(runif(100, min = 0, max = 3))
-
+df.full[1, 2:8] <- 1
 
 #' data frame with missing values
 #' @export
@@ -31,10 +33,11 @@ df.na <- df.full
 for (i in 2:ncol(df.na)){
 df.na[sample(2:nrow(df.na), 10), i] <- NA
 }
+rm(i)
 
 #' Regression weights for SGRQ items
 #'
 #' A data frame containing the regression weights for SGRQ items
 #'
-#' @format A data frame with 51 variables (id, sgrq.1 -- sgrq.50) and six rows
+#' @format A data frame with 51 variables (id, sgrq.1 -- sgrq.17) and six rows
 "reg.weights"
